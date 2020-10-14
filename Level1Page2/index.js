9cceaa761abee2ffe64c846aac55f77eb6ca4ec7
Raksha <57195964+raksha009@users.noreply.github.com>
2020-10-14 13:01:40 +0530

    Level 1 and Leve2 added

---
+++ Level1Page2/index.js
@@ -0,0 +1,32 @@
+
+var flag = 0;
+var timeLeft = 35;
+    var elem = document.getElementById('timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+        if (flag != 1){
+          if (timeLeft == -1) {
+            clearTimeout(timerId);
+            elem.innerHTML = "Time's Up!";
+            document.getElementById('time').style.display = "block";
+          } else {
+            elem.innerHTML = timeLeft + 'seconds remaining!';
+            timeLeft--;
+          }
+        }
+    }
+
+
+function validate1(){
+ var input1 = document.getElementById('answer1').value;
+ if(input1.toUpperCase() == "BARNEY"){
+     document.getElementById('correct').style.display = "block";
+     flag = 1;
+ }
+ else{
+     document.getElementById('wrong').style.display = "block";
+     flag = 1;
+ }
+}
